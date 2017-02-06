53c67f1f875359695b2f0cffeef82f1ef94536e1
Quyen Le <quyen.lechi@gmail.com>
2017-02-06 14:26:17 +0700

    Initial Commit

---
+++ MovieBooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieBooking
+//
+//  Created by Chi-Quyen Le on 2/6/17.
+//  Copyright © 2017 Chi-Quyen Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MovieBookingTests/MovieBookingTests.swift
@@ -0,0 +1,36 @@
+//
+//  MovieBookingTests.swift
+//  MovieBookingTests
+//
+//  Created by Chi-Quyen Le on 2/6/17.
+//  Copyright © 2017 Chi-Quyen Le. All rights reserved.
+//
+
+import XCTest
+@testable import MovieBooking
+
+class MovieBookingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
